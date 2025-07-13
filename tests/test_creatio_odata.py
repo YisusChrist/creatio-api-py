54a4bb39863ebf25de0653353371dddbb70c070e
Alejandro González Momblán <58041337+YisusChrist@users.noreply.github.com>
2025-07-13 20:58:56 +0200

    tests: update tests module to work with new implementation

--- tests/test_creatio_odata.py
+++ tests/test_creatio_odata.py
@@ -2,16 +2,19 @@
 
 import pytest
 import requests
+from pydantic import HttpUrl
 
 from creatio_api_py import CreatioODataAPI
 
 
+creatio_url = "https://davissamac.creatio.com"
+main_collection = "Case"
 record_id: str = ""
 
 
 @pytest.fixture
 def api() -> CreatioODataAPI:
-    base_url = "https://davissamac.creatio.com"
+    base_url = HttpUrl(creatio_url)
     # Initialize the CreatioODataAPI instance with a custom base URL
     api = CreatioODataAPI(base_url=base_url)
     api.authenticate()
@@ -26,7 +29,7 @@ def test_init_failure() -> None:
 
 def test_init(api: CreatioODataAPI) -> None:
     # Test the __init__ method
-    assert api.base_url == "https://davissamac.creatio.com"
+    assert api.base_url == HttpUrl(creatio_url)
 
 
 def test_number_of_api_calls(api: CreatioODataAPI) -> None:
@@ -36,40 +39,69 @@ def test_number_of_api_calls(api: CreatioODataAPI) -> None:
 
 def test_make_request(api: CreatioODataAPI) -> None:
     # Test the make_request method
-    response = api._make_request("GET", "/0/odata/AcademyURL")
-    assert response.status_code == 200
+    with pytest.raises(AttributeError) as e:
+        api._make_request("GET", "/0/odata/AcademyURL")
+    assert "object has no attribute '_make_request'" in str(e.value)
 
 
-def test_authentication(api: CreatioODataAPI) -> None:
+def test_authentication_no_cache(api: CreatioODataAPI) -> None:
     # Test the authentication method
-    response = api.authenticate()
-    assert response.status_code == 200
+    response = api.authenticate(cache=False)
+    assert response.status_code == 200  # Assuming the authentication is successful
+
+
+def test_authentication_cache(api: CreatioODataAPI) -> None:
+    # Test the authentication method
+    response = api.authenticate(cache=True)
+    assert response.status_code == None  # No response expected when using cache
 
 
 def test_authentication_failure(api: CreatioODataAPI) -> None:
     with pytest.raises(requests.exceptions.SSLError) as e:
         # Test the authentication method with invalid credentials
-        api.base_url = "https://invalid_url.creatio.com"
+        api.base_url = HttpUrl("https://invalid_url.creatio.com")
         api.authenticate()
 
-    assert "HTTPSConnectionPool(host=\\'invalid_url.creatio.com\\', port=443)" in str(e)
+    assert "host=\\'invalid_url.creatio.com\\', port=443)" in str(e)
+
+
+def test_get_collection_error_wrong_collection(api: CreatioODataAPI) -> None:
+    # Test to retrieve a collection that does not exist
+    collection = "InvalidCollection"
+    with pytest.raises(requests.exceptions.HTTPError) as e:
+        api.get_collection_data("InvalidCollection")
+    assert (
+        f"401 Client Error: Unauthorized for url: {creatio_url}/0/odata/{collection}"
+        in str(e.value)
+    )
+
+
+def test_get_collection_error_over_20k(api: CreatioODataAPI) -> None:
+    # Test to retrieve a collection with more than 20,000 records
+    with pytest.raises(requests.exceptions.HTTPError) as e:
+        api.get_collection_data(main_collection)
+    assert (
+        f"500 Server Error: Internal Server Error for url: {creatio_url}/0/odata/{main_collection}"
+        in str(e.value)
+    )
 
 
 def test_get_collection_data(api: CreatioODataAPI) -> None:
-    # Test the get_collection_data method
-    response = api.get_collection_data("Case")
+    # Test to retrieve one record from an existing collection
+    response = api.get_collection_data(main_collection, top=1)
     assert response.status_code == 200
 
 
 def test_create_record(api: CreatioODataAPI) -> None:
     global record_id
+
     # Test the add_collection_data method
     data: dict[str, str] = {
         "UsrEmail": "test@test.com",
         "UsrTelefono": "123456789",
         "UsrDescripcionBienContratado": "Test",
     }
-    response = api.add_collection_data("Case", data)
+    response = api.add_collection_data(main_collection, data)
     assert response.status_code == 201
 
     # Retrieve the ID of the newly created record from the response JSON
@@ -80,23 +112,19 @@ def test_create_record(api: CreatioODataAPI) -> None:
 
 
 def test_modify_collection_data(api: CreatioODataAPI) -> None:
-    global record_id
     # Test the modify_collection_data method
     data: dict[str, str] = {"UsrEmail": "new_email@test.com"}
     response = api.modify_collection_data(
-        collection="Case", record_id=record_id, data=data
+        collection=main_collection, record_id=record_id, data=data
     )
     assert response.status_code == 204
 
 
 def test_delete_collection_data(api: CreatioODataAPI) -> None:
-    global record_id
     # Test the delete_collection_data method
-    response = api.delete_collection_data(collection="Case", record_id=record_id)
-    if response.status_code == 500:
+    with pytest.raises(requests.exceptions.HTTPError) as e:
+        api.delete_collection_data(collection=main_collection, record_id=record_id)
         assert (
-            'update or delete on table "Case" violates foreign key constraint'
-            in response.json()["error"]["innererror"]["message"]
+            f'update or delete on table "{main_collection}" violates foreign key constraint'
+            in str(e.value)
         )
-    else:
-        assert response.status_code == 204
